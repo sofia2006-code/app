523e40cf100d87c6360c92f12ed4c36479942404
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-06-21 17:19:54 -0300

    feat: change state

--- components/timer.js
+++ components/timer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-export default function Timer() {
+export default function Timer({stage, switchStage, getTickingTime}) {
   const options = ["Pomodoro", "Short Break", "Long Break"]
   return (
-      <div className="w-10/12 mx-auto pt-5 text-white flex justify-center intems-center mt-10">
-        <div className="flex gap-5 items-center">
+      <div className="w-10/12 mx-auto pt-5 text-white flex flex-col justify-center intems-center mt-10">
+        <div className="flex gap-5 items-centerflex-col justify-center align-items">
           {options.map((option, index) => {
             return(
-            <h1 key={index} className= {` ${index ===}p-1 cursor-pointer transition-all`}>
+            <h1 key={index} className= {` ${index === stage ?"bg-gray-500 bg-opacity-30":""
+            }p-1 cursor-pointer transition-all rounded`}
+              onClick={() => switchStage(index)}
+            >
                 {option}
               </h1>
             );
@@ -16,7 +19,17 @@ export default function Timer() {
 
         </div>
         
-
+        <div className="mt-10 mb-10">
+          <h1 className="text-8xl font-bold select-none m-0 flex flex-col justify-center items-center">
+            {getTickingTime()}:00
+          </h1>
+        </div>
+        <div className="flex gap-2 items-center flex-col justify-center">
+          <button className="px-16 py-2 text-2xl rounded-md bg-white text-blue-500 uppercase font-bold">
+            Start
+          </button>
+        </div>
+          
       </div>
   )
 }
--- pages/pomodoro.js
+++ pages/pomodoro.js
@@ -1,16 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import Navigations from '../components/navigations'
 import Timer from '../components/timer'
 import styles from '../styles/Home.module.css'
+
 //import styles from '../styles/globals.css'
 
 export default function Home() {
+  const [pomodoro, setPomodoro] = useState(25);
+  const [shortBreak, setShortBreak] = useState(5);
+  const [longBreak, setLongBreak] = useState(40);
+
+  const[stage, setStage] = useState(0)
+
+  const switchStage = (index) =>{
+    setStage(index);
+  }
+
+  const getTickingTime = ()=> {
+    const timeStage ={
+      0: pomodoro,
+      1: shortBreak,
+      2: longBreak,
+    }
+
+     return timeStage[stage]; 
+  }
   return (
     <div className="bg-gray-900 min-h-screen font-inter">
       <div className="max-w-2xl min-h-screen mx-auto">
         <Navigations/>
-        <Timer/>
+        <Timer stage={stage} switchStage={switchStage} getTickingTime={getTickingTime}/>
       </div>
     </div>
   )
