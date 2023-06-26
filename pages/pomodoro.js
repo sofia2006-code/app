66e99b33e0b2a58ab6b00b94ce423a5624161b86
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-06-26 07:26:33 -0300

    feat: started alarm

--- pages/pomodoro.js
+++ pages/pomodoro.js
@@ -1,21 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Navigations from '../components/navigations'
 import Timer from '../components/timer'
 import styles from '../styles/Home.module.css'
+import Alarm from '../components/alarm'
 
 //import styles from '../styles/globals.css'
 
 export default function Home() {
   const [pomodoro, setPomodoro] = useState(25);
-  const [shortBreak, setShortBreak] = useState(5);
+  const [shortBreak, setShortBreak] = useState(1);
   const [longBreak, setLongBreak] = useState(40);
   const[seconds, setSeconds] = useState(0);
   const[stage, setStage] = useState(0);
   const [consumedSecond, setConsumeSec] = useState(0)
   const [ticking, setTicking] = useState(false);
 
+  const alarmRef = useRef();
+
+  //cambia de estado cuando se consumen los segundos 
   const switchStage = (index) =>{
     const isYes = consumedSecond && stage !== index 
       // ? confirm("Cambiar de estado")
@@ -50,17 +54,26 @@ export default function Home() {
     return updateStage[stage];
   }
 
+  // setea todos los valores a su valor original
   const reset =() =>{
     setTicking(false);
     setPomodoro(25);
     setLongBreak(10);
-    setShortBreak(5);
+    setShortBreak(1);
     setSeconds(0);
+  }
+  
+  //cuando hay que resetear le da play a la alarma
+  const timeUp = () =>{
+    reset();
+    setIsTimeUp(true);
+    alarmRef.current.play
   }
   const clockTicking =()=>{
     const minutes =getTickingTime();
     const setMinutes = updateminute();
 
+    // TIME UP
     if (minutes === 0 && seconds === 0){
       reset();
     }
@@ -73,6 +86,10 @@ export default function Home() {
     }
   }
   useEffect(() =>{
+
+    window.onbeforeunload = () =>{
+      return consumedSecond ? "Show waring" : null;
+    };
     const timer = setInterval(() => {
       if(ticking){
         setConsumeSec(value => value + 1)
@@ -96,6 +113,7 @@ export default function Home() {
          seconds={seconds}
          ticking={ticking}
          setTicking={setTicking}/>
+         <Alarm ref={alarmRef}/>
       </div>
     </div>
   )
