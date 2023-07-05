f336f7fd856f2fe5706c8e3f03ba40bb74943dd7
sofia2006-code <75650383+sofia2006-code@users.noreply.github.com>
2023-07-05 14:30:25 -0300

    Form Task and Timer designs done

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -21,14 +21,12 @@ function TasksPage() {
 
     return (
     <>
-        <div class=" py-12 px-8 max-w-sm mx-auto rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:space-y-0 sm:space-x-6 bg-slate-500 text-white "> 
-                <div class=" flex justify-between space-y-0">
-                    <label class="flex gap-5 text-lg text-white font-semibold">Asignar Tarea</label>
-                    <input class="  text-slate-500"type='text' value={task} onChange={e => setTask(e.target.value)}></input>
-                </div>
+        <div class="m-auto w-[50vh] h-[20vh] min-h-[11em] flex flex-col rounded-[1.5em] bg-slate-500 text-white "> 
+                <label class="ml-10 mt-5 flex mr-1 gap-5 text-lg text-white font-semibold">Asignar Tarea</label>
+                <input class="text-slate-500 w-[12em] mt-5 ml-11"type='text' value={task} onChange={e => setTask(e.target.value)}></input>
                 
             <div class="relative">
-                <button onClick={submitTask} className = ' px-4 py-1 text-sm text-white-600 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2'>Submit Task</button>
+                <button onClick={submitTask} className = 'mt-10 ml-11 px-4 py-1 text-sm text-white-600 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2'>Submit Task</button>
    
             </div>
 
--- pages/timers/index.js
+++ pages/timers/index.js
@@ -7,6 +7,10 @@ function TimersPage() {
     const [timerWork, setTimerWork] = useState('')
     const [RestTime, setRestTime] = useState('')
 
+    const handletimerWork = ()=> {
+
+    }
+
     //no lo toques
     const fetchTimers = async () => {
         const response = await fetch('/api/timers')
@@ -29,36 +33,29 @@ function TimersPage() {
 
     return (
         <>
-            <div>
-                <h2> Dato: Ingrese la tarea</h2>
-                <input type='text' value={dato} onChange={e => setDato(e.target.value)}></input>
+            <div class="m-auto w-[50vh] h-[40vh] min-h-[55vh]  flex flex-col rounded-[1.5em] bg-slate-500 text-white ">
+                <div>
+                    <h2 class="ml-10 mt-5 flex mr-1 gap-5 text-lg text-white font-semibold"> Dato: Ingrese la tarea</h2>
+                    <input type='text' value={dato} onChange={e => setDato(e.target.value)} class="text-slate-500 w-[12em] mt-5 ml-11"></input>
+                </div>
+
+                <div>
+                    <h2 class="ml-10 mt-5 flex mr-1 gap-5 text-lg text-white font-semibold"> Ingrese las horas de trabajo</h2>
+                    <input type='time' value={timerWork} onChange={e => setTimerWork(e.target.value)} class="text-slate-500 w-[12em] mt-5 ml-11"></input>
+                </div>
+
+                <div>
+                    <h2 class="ml-10 mt-5 flex mr-1 gap-5 text-lg text-white font-semibold"> Ingrese las horas de recreo</h2>
+                    <input type='time' value={RestTime} onChange={e => setRestTime(e.target.value)} class="text-slate-500 w-[12em] mt-5 ml-11"></input>
+                </div>
+
+                <div class="relative">
+                    <button onClick={submitTimer} className='mt-10 ml-[3em] px-4 py-1 text-sm text-white-600 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2'>Submit Timer</button>
+                    
+                </div>
+
             </div>
-
-            <div>
-                <h2> Ingrese las horas de trabajo</h2>
-                <input type='number' value={timerWork} onChange={e => setTimerWork(e.target.value)}></input>
-            </div>
-
-            <div>
-                <h2> Ingrese las horas de recreo</h2>
-                <input type='number' value={RestTime} onChange={e => setRestTime(e.target.value)}></input>
-            </div>
-
-            <div class="relative h-32 w-32">
-                <button onClick={submitTimer} className='absolute inset-x-0 bottom-0 h-16'>Submit Timer</button>
-
-                <button onClick={fetchTimers} className='absolute inset-x-0 bottom-0 h-0'>Load Timers</button>
-                {
-                    timers.map(timer => {
-                        return (
-                            <div key={timer.id}>
-                                {timer.id} {timer.dato} {timer.timerWork} {timer.RestTime}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-
+           
         </>
     )
 
