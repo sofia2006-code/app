8f40b47d433c9731a6e2e76f6ce82fa67bab8fd1
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-08-01 12:09:22 -0300

    feat: started index

--- components/navigations.js
+++ components/navigations.js
@@ -1,14 +1,15 @@
 
 import React from 'react'
-import { FiBell, FiSettings } from "react-icons/fi";
+import { FiArrowLeft, FiSettings } from "react-icons/fi";
 export default function Navigations({setOpenSettings}) {
   return (
       <nav className="pt-5 text-white flex justify-between w-11/12 mx-auto">
         
         <div className="flex items-center gap-1 cursor-pointer">
-          <FiBell className="text-sm" />
+          <FiArrowLeft className="text-sm" />
           <h1 >Tafocus</h1>
         </div>
+        {/* meter aca un booleano para usarlo en el index */}
         <FiSettings className="text-2xl cursor-pointer" onClick={()=>setOpenSettings(value =>!value)}/>
       </nav>
   )
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-
-
-      <h1 className="bg-red-900">aprendiendo esta mierda</h1>
+    <div className="bg-black min-h">
+      
     </div>
   )
 }
