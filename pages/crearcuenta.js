29eb6740e70759ec7aa6e3b3160e797f53455d9a
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-08-08 08:27:34 -0300

    fixed: crearcuenta distribution

--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -8,29 +8,26 @@ import { AiOutlineEyeInvisible } from "react-icons/ai"
 
 export default function Home() {
   return (
+    <div className="bg-gradient-to-r from-[#1B153F] to-[#1B153F] min-h-screen flex flex-col justify-start items-start pb-10">
+  <Navigations openConf={false} />
+  <h1 className="font-medium text-5xl px-8 pt-10 pb-3 text-white">Crea tu cuenta</h1>
+  <div className="flex flex-col justify-start items-start">
+    <input className="mx-8 my-14 shadow-inner appearance-none border-none rounded w-full py-2 px-3 bg-gray-400 bg-opacity-30 text-gray-200 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+    <input className="mx-8 shadow-inner appearance-none border-none rounded w-full py-2 px-3 bg-gray-400 bg-opacity-30 text-gray-200 mb-4 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+  </div>
+  <div className="mt-4 flex items-center text-[#DF6B00]">
+    <input type="checkbox" className="h-4 w-4 rounded-md border-[#DF6B00] focus:ring-0 focus:shadow-none" />
+    <span className="ml-2 text-white">Permanecer logueado</span>
+  </div>
+  <button className="mt-6 px-10 py-3 text-lg font-semibold rounded-full bg-[#DF6B00] text-white">
+    Crear cuenta
+  </button>
+  <div className="flex items-center w-full mt-6">
+    <hr className="flex-grow border-t border-gray-400" />
+    <span className="mx-2 text-white font-semibold text-xl">o</span>
+    <hr className="flex-grow border-t border-gray-400" />
+  </div>
+</div>
 
-    <div className="bg-gradient-to-r from-[#1B153F] to-[#1B153F]  min-h-screen min-w-screen flex flex-col  items-center">
-      <Navigations openConf={false}/>
-      <h1 className ="font-medium text-5xl px-8 py-10 text-white"> Crea tu cuenta </h1>
-      <button className="r my-8 px-10 py-2 text-lg rounded-md bg-gray-400 bg-opacity-30 text-stone-200  flex text-left
-      font-normal"> <FiMail className='mr-2'/>Email</button> 
-      <button className=" px-7 py-2 text-lg rounded-md bg-gray-400 bg-opacity-30 text-stone-200 flex text-left
-      font-normal"> <BsFillLockFill className="mr-2"/>Contraseña</button> 
-      <div>
-      <button className="flex  items-center w-8 h-8 border-2 border-[#DF6B00]"/> Permanecer logueado
-      </div>
-      <button className="mt-14 px-[80px] py-3 text-lg rounded-full bg-[#DF6B00] text-white font-mormal">
-            Crear cuenta      
-      </button>
-      <div className="flex items-center w-full mt-10">
-        <hr className="flex-grow border-t border-gray-400" />
-        <span className="mx-4 text-white font-semibold text-xl">o</span>
-        <hr className="flex-grow border-t border-gray-400" />
-      </div>
-      {/* <div className="absolute bottom-0 right-0">
-        <img src={Woman} alt="Background" />
-      </div> */}
-    </div>
-    
   )
 }
