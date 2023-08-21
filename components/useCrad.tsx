9f1ff61183786fcbb1fb572c61d145d29f7dd388
sofia2006-code <75650383+sofia2006-code@users.noreply.github.com>
2023-08-21 19:04:07 -0300

    UseCard

---
+++ components/useCrad.tsx
@@ -0,0 +1,9 @@
+export function user() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <p>Current Log </p>
+            </div>
+        </div>
+    )
+}
