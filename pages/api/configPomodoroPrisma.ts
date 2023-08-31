8bed50df6e5217b8ad975cf5869873f255782bf6
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2023-08-31 09:04:56 -0300

    tareas y timers unicos, subida de tareas y timers andando

--- pages/api/configPomodoroPrisma.ts
+++ pages/api/configPomodoroPrisma.ts
@@ -1,11 +1,11 @@
 //imports
-import { PrismaClient } from ".prisma/client";
+import { PrismaClient, Prisma } from ".prisma/client";
 import { authOptions } from "../api/auth/[...nextauth]"
 import { getServerSession } from "next-auth/next"
 
 
 //variables
-const prisma = new PrismaClient;
+const client = new PrismaClient();
 
 let sesion: [string];
 
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
     const session = await getServerSession(req, res, authOptions);
     
    //Conseguir usuario loggeado
-    const usuario = await prisma.user.findFirst ({
+    const usuario = await client.user.findFirst ({
         where: {
             email: session.user.email,
         }
@@ -54,33 +54,63 @@ export default async function handler(req, res) {
   
     //crear tarea/timer
     if (req.method === "POST") {
-        //res.status(200);
 
-        //crear tarea (no esta andando)
+        //crear tarea
         if (req.body.tipo == "tarea") {
             
-            const crearTarea = await prisma.tareasPomodoro.create({
-                data: {
-                    userId: usuario.id,
-                    tarea: req.body.dato as string, 
-                 },
-            })
-            console.log(crearTarea);
-            res.status(200);
-            //res.status(200).json(crearTarea);
+            try {
+                const crearTarea = await client.tareasPomodoro.create({
+                    data: {
+                        userId: usuario.id,
+                        tarea: req.body.dato as string, 
+                        completado: false,
+                     },
+                })    
+                res.status(200);
+            } 
+            //Tirar error si tarea ya existe
+            catch (e) {
+                if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                  // The .code property can be accessed in a type-safe manner
+                  if (e.code === 'P2002') {
+                    console.log(
+                      'Tunique constraint violation, la tarea ya existe'
+                    )
+                    res.status(400);
+                  }
+                }
+                //throw e
+            }
         }
 
         //crear timer
         else if (req.body.tipo == "timer") {
+            
+            try {
+                const creatTiempo = await client.tiempoPomodoro.create({
+                    data: {
+                        userId: usuario.id,
+                        nombre: req.body.dato as string,
+                        tiempoTrabajo: req.body.timerWork as string,
+                        tiempoRecreo: req.body.RestTime as string, 
+                     },
+                })
+                res.status(200);
+            } 
+            //Tirar error si timer ya existe
+            catch (e) {
+                if (e instanceof Prisma.PrismaClientKnownRequestError) {
+                  // The .code property can be accessed in a type-safe manner
+                  if (e.code === 'P2002') {
+                    console.log(
+                      'Tunique constraint violation, el timer ya existe'
+                    )
+                    res.status(400);
+                  }
+                }
+                //throw e
+            }
 
-            await prisma.tiempoPomodoro.create({
-                data: {
-                    userId: usuario.id,
-                    nombre: req.body.dato as string,
-                    tiempoTrabajo: req.body.tiempoTrabajo as string,
-                    tiempoRecreo: req.body.tiempoRecreo as string, 
-                 },
-            })
         }
     }
 
