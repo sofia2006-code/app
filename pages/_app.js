5b371dbfdfaaed79380983f1b1c26982f57bd69f
sofia2006-code <sofia.ines.casares@gmail.com>
2023-09-25 23:57:15 -0300

    todolist and calendar head

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
 import PageLayout from '../components/PageLayout'
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
-
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}) {
+}) 
+
+{
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </SessionProvider>    
     //<PageLayout />
   )
 }
--- pages/todolist.js
+++ pages/todolist.js
@@ -1,18 +1,78 @@
 import { useState } from "react";
+import axios from 'axios';
 
-export default function todolist(){
-    const [todos, setTodos] = useState([]);
+const url = "http://localhost:3000/api/toDolist";
+
+export default function todolist(props){
+    const [todos, setTodos] = useState(props.todos);
     const [todo, setTodo] = useState({todo: ""});
     
+    const handlerChange = ({currentTarget: input}) => {
+        input.value === ""
+        ? setTodo({todo: ""})
+        : setTodo((prev) => ({...prev, todo:input.value}));
+    }
+
+    const editTodo = (id) => {
+        const currentTodo = todos.filter((todo) => todo._id === id);
+        setTodo(currentTodo[0]);
+    }
+
+    const addTodo = async(e) => {
+        e.preventDefault();
+        try {
+            if(todo._id){
+                const{data} = await axios.put(url + "/" + todo._id, {todo: todo.todo});
+                const originalTodos = [...todos];
+                const index = originalTodos.findIndex((t) => t.id === todo._id);
+                originalTodos[index] = data.data;
+                setTodos(originalTodos);
+                setTodo({todo: ""});
+                console.log(data.message);
+            }else{
+                const{data} = await axios.post(url, todo);
+                setTodos((prev) =>[...prev, data.data]);
+                setTodo({todo: ""});
+                console.log(data.message)
+            }
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    const updateTodo = async(id) => {
+        try{
+            const originalTodos = [...todos];
+            const index = originalTodos.findIndex((t) => t.id === todo._id);
+            const {data} = await axios.put(url + "/" + id, {completed: !originalTodos[index].completed});
+            originalTodos[index] = data.data;
+            setTodos(originalTodos);
+            console.log(data.message);
+        }catch(error) {
+            console.log(error)
+        }
+    }
+
+    const deleteTodo = async (id) => {
+        try{
+            const {data} = await axios.delete(url + "/" + id);
+            setTodos((prev) => prev.filter((todo) => todo._id !== id));
+            console.log(data.message);
+        }catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <main>
             <h1>TO-DO</h1>
             <div>
-                <form>
+                <form onSubmit={addTodo}>
                     <input
                         type="text"
                         placeholder="Taks to be done..."
-                        //value={todo.todo}
+                        onChenge={handlerChange} 
+                        value={todo.todo}
                         //SET VALUE OF INPUT NOT READ ONLY (buscar esto en internet)
                     />
                 </form>
@@ -25,6 +85,7 @@ export default function todolist(){
                             type="checkbox"
                             className={styles.check_box}
                             checked={todo.completed}
+                            onChange = {() => updateTodo(todo._id)}
                         />
                         <P
                         className={
@@ -33,7 +94,7 @@ export default function todolist(){
                                 : styles.todo_text
                         }>{todo.todo}
                         </P>
-                        <button > &#9998; </button>
+                        <button onClick={() => editTodo(todo._id)} > &#9998; </button>
                         <button > &#10006;</button>
                     </div>
                 ))}
@@ -41,4 +102,14 @@ export default function todolist(){
             </div>
         </main>
     )
+}
+
+
+export const getServerSideProps = async() => {
+    const {data} = await axios.get(url);
+    return{
+        props: {
+            todos: data.data
+        }
+    }
 }
