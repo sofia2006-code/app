44d183cf41bdf695f372ddb90d813fd5b4f1352c
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2023-10-04 20:53:40 -0300

    prueba firebase para pwa

---
+++ pages/api/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDuXf3ZrVRaQnaSswJY9yuzFPFiVgs7G54",
+  
+    authDomain: "pwa-cofocus.firebaseapp.com",
+  
+    projectId: "pwa-cofocus",
+  
+    storageBucket: "pwa-cofocus.appspot.com",
+  
+    messagingSenderId: "483956566609",
+  
+    appId: "1:483956566609:web:1b9cf5c78dec8520f3a670"
+  
+  };
+  
+  
+  // Initialize Firebase
+  
+  const app = initializeApp(firebaseConfig);
+
+export default firebase;
+
