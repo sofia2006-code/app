64e4b41c9938ebcde536d72ab95dff0c5d83dd4d
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-11-21 09:34:40 -0300

    config design

--- components/navigations.js
+++ components/navigations.js
@@ -3,9 +3,9 @@ import React from 'react'
 import { FiArrowLeft, FiSettings } from "react-icons/fi";
 export default function Navigations({setOpenSettings, openConf}) {
   return (
-      <nav className="pt-5 text-white flex justify-between w-11/12 mx-auto">
+      <nav className="pt-3 text-white flex justify-between w-11/12 mx-auto qcy4">
         
-        <div className="flex items-center gap-1 cursor-pointer">
+        <div className="flex items-center gap-1 cursor-pointer qcy5">
           <FiArrowLeft className="text-sm" />
           <h1 >Tafocus</h1>
         </div>
