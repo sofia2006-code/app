c6e5e9c0c2e649f48b3d9f0aef747a5a6114a2db
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2023-11-26 20:13:51 -0300

    Update index.js

--- pages/randomtasks/index.js
+++ pages/randomtasks/index.js
@@ -89,7 +89,12 @@ export default function RandomTasks() {
 
   return (
     <>
+<<<<<<< Updated upstream
       <div className="bg-gradient-to-b from-[#1D1261] to-[#1B153F] font-Quattrocento h-screen w-screen text-white flex flex-col ">
+=======
+   
+   <div className="bg-gradient-to-b from-[#2200FF] to-[#7795ff] grid align-items-center gap-3 mr-8 ml-8 my-10 py-4 shadow-inner appearance-none border-none rounded  px-3 text-gray-200 leading-tight focus:outline-none focus:shadow-outline  flex-col items-center h-auto">
+>>>>>>> Stashed changes
         <Navigations />
         <h1 className="text-2xl text-center pt-14 mt-5 font-bold ">Tareas Espontaneas</h1>
         <p className="text-lg text-center mx-8 px-3 my-4 font-semibold">Cumpli con los descansos para mantener la concentracion</p>
@@ -115,8 +120,13 @@ export default function RandomTasks() {
             </div>
           )}
         </div>
+<<<<<<< Updated upstream
       </div>
       <Footer />
+=======
+      <Footer buttonAppear={false}
+      setFormVisible={setFormVisible}/>
+>>>>>>> Stashed changes
     </>
   );
 }
